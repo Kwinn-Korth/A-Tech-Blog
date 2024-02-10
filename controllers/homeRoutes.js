6dd488173482cdb250ac3e616969c5bf52426129
Kwinn-Korth <kjkorth12@gmail.com>
2024-02-10 16:00:59 -0700

    made homeRoutes, needs to be triple checked and tested

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Function/ Route to get all posts
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+        const posts = postData.map((post) => post.get({ plain: true }));
+        res.render('homepage', { posts, logged_in: req.session.logged_in });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+// Function/ Route to get a single post
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment,
+                    include: [
+                        {
+                            model: User,
+                            attributes: ['username'],
+                        },
+                    ],
+                },
+            ],
+        });
+
+        const post = postData.get({ plain: true });
+        res.render('single-post', { post, logged_in: req.session.logged_in });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+// Function/ Route to get the user profile page
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
+        });
+
+        const user = userData.get({ plain: true });
+        res.render('profile', { ...user, logged_in: true });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
+
+// Function/ Route to get the login page
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
