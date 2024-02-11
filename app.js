cde381d0a79a337a23ea7ec753410c99590dc58c
Kwinn-Korth <kjkorth12@gmail.com>
2024-02-10 17:48:17 -0700

    added seeds so it doesn't look empty

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./routes');
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
